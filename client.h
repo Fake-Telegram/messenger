8997278b926ea9d0f096794984ce5a418686e2e3
Kapitonov <kapitonov.a.e@yandex.ru>
2016-10-09 18:12:20 +0300

    хоть что-то

--- client.h
+++ client.h
@@ -2,6 +2,18 @@
 #define CLIENT_H
 
 #include <string>
+#include <list>
+#include "user.h"
+
+class Client
+{
+public:
+    Client();
+    Client(const Client &other);
+    ~Client();
+private:
+    string password;
+    list<User> friends;
 };
 
 #endif // CLIENT_H
