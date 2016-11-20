cdb8c0972fae9321948a2d2519b9469fb1630cb0
Kapitonov <kapitonov.a.e@yandex.ru>
2016-11-20 18:57:30 +0300

    7

--- chat.h
+++ chat.h
@@ -1,34 +1,4 @@
-<<<<<<< HEAD
 //#pragma once
-#ifndef CHAT_H
-#define CHAT_H
-
-#include <list>
-#include "user.h"
-#include "message.h"
-#include "fstream"
-
-class Chat
-{
-private:
-	User companion;
-	bool otr_status;
-	list <Message> talk;
-	//Criptographer my_otr;
-public:
-	Chat(const User&);
-	~Chat();
-	bool send_message(const string&);
-	bool recv_message(const string&);
-	void change_otr_status();
-	list <Message> find_message(const string&);
-	bool operator==(const Chat& right);
-};
-
-#endif // CHAT_H
-=======
-//#pragma once
-#ifndef CHAT_H
 #define CHAT_H
 
 #include <list>
@@ -57,4 +27,3 @@ public:
 };
 
 #endif // CHAT_H
->>>>>>> sec
--- client.h
+++ client.h
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-//#pragma once
-#ifndef CLIENT_H
-#define CLIENT_H
-
-#include <list>
-#include "user.h"
-#include "chat.h"
-
-class Client : public User{
-private:
-	string password;
-	list<User> friends;
-	list<Chat> room;
-public:
-    Client(const string&, const string&, const string&);
-	~Client();
-    bool add_friend(const User&);
-	bool delete_friend(const User&);
-	bool add_chat(const User&);
-	bool open_chat(const Chat&);//??????
-	bool close_chat(const Chat&);//??????
-	bool delete_chat(const User&);
-    bool change_password(const string&, const string&);
-    bool autorization(const string&);
-};
-#endif // CLIENT_H
-=======
 //#pragma once
 #ifndef CLIENT_H
 #define CLIENT_H
@@ -55,4 +27,3 @@ public:
     bool autorization(const string&);
 };
 #endif // CLIENT_H
->>>>>>> sec
